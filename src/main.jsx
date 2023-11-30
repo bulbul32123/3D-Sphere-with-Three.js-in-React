0d791143069029db0544666f16b289cca2b6b9f4
bulbul32123 <bulbulhero63@gmail .com>
2023-11-30 13:35:09 +0600

    Create 3D Obects in Web

---
+++ src/main.jsx
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+import './index.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Scene
+const scene = new THREE.Scene()
+
+
+// Create our sphere
+
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+    color: '#00ff83',
+})
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+// Sizes
+const sizes= {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+// Light
+const light = new THREE.PointLight(0xffffff, 100, 900)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+
+// Render
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height);
+console.log(renderer.domElement);
+renderer.render(scene,camera)
+renderer.setPixelRatio(2)
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+window.addEventListener('resize',()=>{
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //update Camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = ()=>{
+  controls.update()
+  renderer.render(scene,camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
+
+
+
+
+
+
+
